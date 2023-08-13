934dd319472dd519d53019b5cb23729627f0d87e
tw2818 <93477727+tw2818@users.noreply.github.com>
2023-08-14 02:50:01 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@ export const APP_ID = '9c8d767f-3088-481c-b9f4-6ebecf3a566a'
 export const API_KEY = 'app-j47ksUlx3AdbeOZhCm9VwEch'
 export const API_URL = 'http://43.128.108.81/v1'
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
+  title: 'MedAsk',
   description: 'App description',
   copyright: '',
   privacy_policy: '',
